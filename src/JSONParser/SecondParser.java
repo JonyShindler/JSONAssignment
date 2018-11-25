e31053e2c6124a3ddd6b531976254713d7310aa6
jony <jonathanshindler@hotmail.com>
2018-11-25 23:23:25 +0000

    Added array based parser

--- src/JSONParser/SecondParser.java
+++ src/JSONParser/SecondParser.java
@@ -5,10 +5,35 @@ import java.util.*;
 public class SecondParser {
     public Map<String, String> parse(String json) {
 
-        String[] splitStrings = json.split(",");
+        List<String> splitStrings = new ArrayList<>();
+
+        boolean insideArray = false;
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < json.length(); i++){
+            char c = json.charAt(i);
+            //Dont split on commas if we are inside an array
+            if (c=='[') {
+                insideArray = true;
+                System.out.println("Its an array");
+            } else if (c==']') {
+                insideArray = false;
+                System.out.println("Array over");
+            } else if (!insideArray && c == ',') {
+                //Do a split
+                splitStrings.add(stringBuffer.toString());
+                stringBuffer.delete(0, stringBuffer.capacity());
+            } else {
+                stringBuffer.append(c);
+            }
+            //Process char
+        }
+        splitStrings.add(stringBuffer.toString());
+
+
+//        String[] splitStrings = json.split(",");
         List<String> strings = new ArrayList<>();
 
-        for (String aString : Arrays.asList(splitStrings)) {
+        for (String aString : splitStrings) {
             System.out.println("String is " + aString);
             strings.add(aString);
         }
@@ -17,12 +42,11 @@ public class SecondParser {
         Map<String, String> jsonMap = new HashMap<>();
 
         for (String string : strings) {
-            //TODO dont split on commas if we are inside an array?
             string = string.replaceAll("\"", "");
             String[] split = string.split(":");
             //remove the speech marks from strings for now
 
-//            jsonMap.put(split[0], split[1]);
+            jsonMap.put(split[0], split[1]);
         }
 
 
--- test/JSONParser/SecondParserTest.java
+++ test/JSONParser/SecondParserTest.java
@@ -11,7 +11,7 @@ public class SecondParserTest {
         String json = "{\"id\":\"s113867\",\"task\":\"/task/5447\"}";
         System.out.println("Input JSON is: " + json);
         Map<String,String> map = new SecondParser().parse(json);
-        System.out.println(map.toString());
+        System.out.println("Map is: " + map.toString());
 
     }
 
@@ -20,7 +20,7 @@ public class SecondParserTest {
         String json = "{\"id\":\"s113867\",\"tasks\":[\"/task/5447\",\"/task/2867\",\"/task/8060\"]}";
         System.out.println("Input JSON is: " + json);
         Map<String,String> map = new SecondParser().parse(json);
-        System.out.println(map.toString());
+        System.out.println("Map is: " + map.toString());
 
     }
 
