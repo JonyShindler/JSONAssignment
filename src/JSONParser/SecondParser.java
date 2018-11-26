171e76295ce3e7ca1b7a494b28183552a5ed07ec
JonyShindler <37512517+JonyShindler@users.noreply.github.com>
2018-11-26 11:01:13 +0000

    Added map work into main loop so there is no seperate string list.

--- src/JSONParser/SecondParser.java
+++ src/JSONParser/SecondParser.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class SecondParser {
     public Map<String, String> parse(String json) {
 
-        List<String> splitStrings = new ArrayList<>();
+        Map<String, String> jsonMap = new TreeMap<>();
 
         boolean insideArray = false;
         StringBuffer stringBuffer = new StringBuffer();
@@ -18,29 +18,28 @@ public class SecondParser {
                 insideArray = false;
             } else if (!insideArray && c == ',') {
                 //Do a split
-                splitStrings.add(stringBuffer.toString());
+                splitAroundColonAndAddToMap(stringBuffer.toString(), jsonMap);
+                stringBuffer.delete(0, stringBuffer.capacity());
+            } else if (c == '}') {
+                //Do a split
+                splitAroundColonAndAddToMap(stringBuffer.toString(), jsonMap);
                 stringBuffer.delete(0, stringBuffer.capacity());
             } else {
                 stringBuffer.append(c);
             }
         }
-        splitStrings.add(stringBuffer.toString());
 
+//TODO we need to handle objects inside objects. each will get its own map
+//TODO we might want to have wrapper objects for the arrays and stuff so they can be iterated over.
 
-        // now put them in the map by splutting them again
-        Map<String, String> jsonMap = new TreeMap<>();
+        return jsonMap;
+    }
 
-        for (String string : splitStrings) {
+    private void splitAroundColonAndAddToMap(String string, Map<String, String> jsonMap) {
             string = string.replaceAll("\"", "");
             String[] split = string.split(":");
             //remove the speech marks from strings for now
 
             jsonMap.put(split[0], split[1]);
-        }
-
-//TODO we need to handle objects inside objects. each will get its own map
-
-
-        return jsonMap;
     }
 }
--- test/JSONParser/SecondParserTest.java
+++ test/JSONParser/SecondParserTest.java
@@ -32,6 +32,15 @@ public class SecondParserTest {
 
     }
 
+    @Test
+    public void testNestedJSON(){
+        String json = "{instruction\":\"concat\",\"parameters\":[6960,\"6475\"],\"response URL\":\"/answer/3215}";
+        System.out.println("Input JSON is: " + json);
+        Map<String,String> map = new SecondParser().parse(json);
+        System.out.println("Map is: " + map.toString());
+
+    }
+
 
 
 }
